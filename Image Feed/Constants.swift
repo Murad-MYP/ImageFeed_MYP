24825f13bc76f90718a117243dee3c8bbb384a7e
Murad-MYP <murad.dz@internet.ru>
2025-05-19 23:26:57 +0300

    Исправление ошибок

--- Image Feed/Constants.swift
+++ Image Feed/Constants.swift
@@ -6,8 +6,8 @@ enum Constants {
     enum API {
         static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
         static let accessScope = "public+read_user+write_likes"
-        static let redirectURI = "<ваш Redirect URI>"
-        static let secretKey = "<ваш Secret Key>"
-        static let accessKey = "<ваш Access Key>"
+        static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+        static let secretKey = "_dG6ylkUDpwmL0mK_b4Ym-fUXm3GFE1l5S0YU1XkfIA"
+        static let accessKey = "tnZWP4_QOjUChKVUe-Sp4dAUJUXTv2oFisJDfZ39OD8"
     }
 }
