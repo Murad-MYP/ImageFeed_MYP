44755a66d32f5d58dd470e6f6e213c3832a22b2e
Murad-MYP <murad.dz@internet.ru>
2025-05-21 08:26:13 +0300

    Ревью 10 спринт

--- Image Feed/ServiceAccessLayer/OAuth2/OAuth2Service.swift
+++ Image Feed/ServiceAccessLayer/OAuth2/OAuth2Service.swift
@@ -1,7 +1,92 @@
 import Foundation
 
-final class OAuth2Service {
-    func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
-        completion(.success("")) // TODO [Sprint 10]
+struct OAuthTokenResponseBody: Decodable {
+    let accessToken: String
+    let tokenType: String
+    let scope: String
+    let createdAt: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case tokenType = "token_type"
+        case scope
+        case createdAt = "created_at"
     }
+}
+
+final class OAuth2Service {
+    static let shared = OAuth2Service()
+    private init() {}
+    
+    private var task: URLSessionTask?
+    private var lastCode: String?
+    
+    func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        assert(Thread.isMainThread)
+        
+        if lastCode == code { return }
+        task?.cancel()
+        lastCode = code
+        
+        let request = makeRequest(code: code)
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+               response.statusCode < 200 || response.statusCode >= 300 {
+                DispatchQueue.main.async {
+                    completion(.failure(NetworkError.httpStatusCode(response.statusCode)))
+                }
+                return
+            }
+            
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(.failure(NetworkError.urlRequestError(URLError(.badServerResponse))))
+                }
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let responseBody = try decoder.decode(OAuthTokenResponseBody.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(responseBody.accessToken))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+        self.task = task
+        task.resume()
+    }
+    
+    private func makeRequest(code: String) -> URLRequest {
+        var urlComponents = URLComponents(string: "\(Constants.API.defaultBaseURL)/oauth/token")!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.API.accessKey),
+            URLQueryItem(name: "client_secret", value: Constants.API.secretKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.API.redirectURI),
+            URLQueryItem(name: "code", value: code),
+            URLQueryItem(name: "grant_type", value: "authorization_code")
+        ]
+        
+        var request = URLRequest(url: urlComponents.url!)
+        request.httpMethod = "POST"
+        return request
+    }
+}
+
+enum NetworkError: Error {
+    case httpStatusCode(Int)
+    case urlRequestError(Error)
 }
